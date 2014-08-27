52e97624b4cf15b8082fd9ab0da971e1e89cb46f
hb <hb@aroma-project.org>
2014-08-26 21:12:25 -0700

    Cleanup

--- build/all.R
+++
@@ -1,7 +0,0 @@
-library("R.rsp")
-
-for (script in c("install", "rfile")) {
-  filename <- sprintf("%s.rsp", script)
-  pathname <- rfile(filename, path="rsp/", workdir="site")
-  print(pathname)
-}
